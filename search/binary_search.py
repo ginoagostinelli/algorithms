5dfff4159ce7c379bf03f6048340cd352cfa7726
Gino Agostinelli <ginoagostinelli5@gmail.com>
2022-06-29 19:16:49 -0300

    binary_search_by_recursion

--- search/binary_search.py
+++ search/binary_search.py
@@ -5,13 +5,13 @@ Time Complexity: O(log(n))
 
 """
 
-def binary_search(array, target) -> int:
+def binary_search(sorted_array: list[int], target: int) -> int:
 
-    left, right = 0, len(array) - 1
+    left, right = 0, len(sorted_array) - 1
 
     while left <= right:
-        mid = (right + left) // 2
-        val = array[mid]
+        mid = left + (right - left) // 2
+        val = sorted_array[mid]
 
         if target < val:
             right = mid - 1
@@ -21,4 +21,19 @@ def binary_search(array, target) -> int:
             return mid
 
     return -1
+
+def binary_search_by_recursion(sorted_array: list[int], target: int, left: int, right: int) -> int:
+
+    if left > right:
+        return -1
+
+    mid = left + (right - left) // 2
+    val = sorted_array[mid]
+    
+    if target < val:
+         return binary_search_by_recursion(sorted_array, target, left, mid - 1)
+    elif target > val:
+         return binary_search_by_recursion(sorted_array, target, mid + 1, right)
+    else:
+        return mid
 
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,4 +1,4 @@
-from search.binary_search import binary_search 
+from search.binary_search import binary_search, binary_search_by_recursion
 import unittest
 
 class TestSearch(unittest.TestCase):
@@ -11,6 +11,16 @@ class TestSearch(unittest.TestCase):
         self.assertEqual(4, binary_search(array, 4))
         self.assertEqual(-1, binary_search(array, 8))
         self.assertEqual(-1, binary_search(array, 25))
+
+        # Test binary_search_by_recursion 
+        l = 0
+        r = len(array) - 1
+
+        self.assertEqual(0, binary_search_by_recursion(array, 1, l, r))
+        self.assertEqual(9, binary_search_by_recursion(array, 20, l, r))
+        self.assertEqual(4, binary_search_by_recursion(array, 4, l, r))
+        self.assertEqual(-1, binary_search_by_recursion(array, 8, l, r))
+        self.assertEqual(-1, binary_search_by_recursion(array, 25, l, r))
 
     if __name__ == '__main__':
         unittest.main()
